a3f1d6762328f4cd6211400dbe1a5f652f541df1
AlexanderNZ <alexcorkin@gmail.com>
2023-03-28 11:43:18 +1300

    extracts component out

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-function Guess() {
-  return <div />;
+function Guess({ userGuess }) {
+    return <>
+        <p key={Math.random() + userGuess} className="guess">
+            {userGuess.map((char) => (<span key={Math.random() + char} className="cell">{char}</span>))}
+        </p>
+    </>;
 }
 
 export default Guess;
--- src/components/GuessLog/GuessLog.js
+++ src/components/GuessLog/GuessLog.js
@@ -1,27 +1,10 @@
 import React from "react";
+import Guess from "../Guess";
 
 function GuessLog({log}) {
     return <>
         <div className="guess-results">
-            {log.map((item) => {
-                if (item !== '') {
-                    return (
-                        <p key={Math.random() + item} className="guess">
-                            {item.map((char) => (<span key={Math.random() + char} className="cell">{char}</span>))}
-                        </p>
-                    )
-                } else {
-                    return (
-                        <p className="guess">
-                            <span className="cell"></span>
-                            <span className="cell"></span>
-                            <span className="cell"></span>
-                            <span className="cell"></span>
-                            <span className="cell"></span>
-                        </p>
-                    )
-                }
-            })}
+            {log.map((item) => ( <Guess key={Math.random() + item} userGuess={item}/> ))}
         </div>
     </>;
 }
