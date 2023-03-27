a28b8bf41d2cdd3b93c342ad6699b7733cb10a57
AlexanderNZ <alexcorkin@gmail.com>
2023-03-28 09:51:33 +1300

    implements guessInput per AC

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 
 function GuessInput() {
-  return <div />;
+    const inputStyle = {
+        textTransform: 'uppercase'
+    }
+
+    function processInput(input) {
+        setGuess(input)
+    }
+
+    function finaliseInput() {
+        const capitalisedInput = guess.toUpperCase()
+        if (capitalisedInput.length < 5) {
+            console.log('Enter exactly 5 characters')
+        } else {
+            console.log(capitalisedInput)
+            setGuess('')
+        }
+    }
+
+    const [guess, setGuess] = useState('')
+    return (
+        <form className="guess-input-wrapper"
+              onSubmit={(event) => {
+                  event.preventDefault();
+                  finaliseInput()
+              }}>
+            <label htmlFor="guess-input">Enter guess:</label>
+            <input
+                id="guess-input"
+                type="text"
+                style={inputStyle}
+                maxLength={5}
+                value={guess}
+                onChange={(event) => {
+                    processInput(event.target.value)
+                }}
+            />
+        </form>
+    );
 }
 
 export default GuessInput;
