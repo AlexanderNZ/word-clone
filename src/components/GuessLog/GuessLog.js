40da38bc2bb7dc4730ebdd0168323422957cdeb0
AlexanderNZ <alexcorkin@gmail.com>
2023-03-28 11:39:57 +1300

    remove extra div

--- src/components/GuessLog/GuessLog.js
+++ src/components/GuessLog/GuessLog.js
@@ -3,27 +3,25 @@ import React from "react";
 function GuessLog({log}) {
     return <>
         <div className="guess-results">
-            <div className="guess-results">
-                {log.map((item) => {
-                    if (item !== '') {
-                        return (
-                            <p key={Math.random() + item} className="guess">
-                                {item.map((char) => (<span key={Math.random() + char} className="cell">{char}</span>))}
-                            </p>
-                        )
-                    } else {
-                        return (
-                            <p className="guess">
-                                <span className="cell"></span>
-                                <span className="cell"></span>
-                                <span className="cell"></span>
-                                <span className="cell"></span>
-                                <span className="cell"></span>
-                            </p>
-                        )
-                    }
-                })}
-            </div>
+            {log.map((item) => {
+                if (item !== '') {
+                    return (
+                        <p key={Math.random() + item} className="guess">
+                            {item.map((char) => (<span key={Math.random() + char} className="cell">{char}</span>))}
+                        </p>
+                    )
+                } else {
+                    return (
+                        <p className="guess">
+                            <span className="cell"></span>
+                            <span className="cell"></span>
+                            <span className="cell"></span>
+                            <span className="cell"></span>
+                            <span className="cell"></span>
+                        </p>
+                    )
+                }
+            })}
         </div>
     </>;
 }
